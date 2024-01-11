f7791e449d4124e57d18aee1323bdcf3e8439f8b
pengsuelee <pengsuelee99@gmai.com>
2024-01-11 20:20:52 +0700

    first commit minipos14

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div><h1>hello vue App V11111</h1></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>
+      hello vue
+    </h1>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Minipos14Home',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
